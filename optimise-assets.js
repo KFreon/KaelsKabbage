aa964671527541635b853731de0a71ccf93eabd9
Kael Larkin <kael.larkin@readify.net>
2019-06-20 13:04:15 +1000

    Adding images for VS post and build scripts

---
+++ optimise-assets.js
@@ -0,0 +1,11 @@
+// Get all processable files, except those already processed
+// Video = ones (avi?) without postfixes
+// Pictures = non-webms
+
+// pictures - convert to webm - need to figure out what the settings for that are
+// Videos - AV1, x264, and VP9
+
+// Move the original somewhere gitignored?
+// Can probably do video and movies in parallel
+
+
