c66e9bfb6a4f453233bda1458d6fa1116955f086
Kael Larkin <kael.larkin@readify.net>
2019-06-20 17:50:09 +1000

    Working on optimisation script

--- optimise-assets.js
+++ optimise-assets.js
@@ -9,3 +9,12 @@
 // Can probably do video and movies in parallel
 
 
+var fs = require('fs');
+
+fs.readdir(path, function(err, items) {
+    console.log(items);
+ 
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
