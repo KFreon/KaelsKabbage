0dba280129216c167f4102c3e48425f4537e87b8
Kael Larkin <Kael.larkin@purple.telstra.com>
2021-02-24 14:15:27 +1000

    Upgrading AssetOptimiser

---
+++ NewAssetOptimiser/VideoJob.cs
@@ -0,0 +1,13 @@
+﻿namespace AssetOptimiser
+{
+    static partial class Program
+    {
+        struct VideoJob
+        {
+            public string FileName { get; set; }
+            public string Directory { get; set; }
+            public Format Format { get; set; }
+            public string DestinationFileName { get; set; }
+        }
+    }
+}
