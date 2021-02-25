9f625451d2b227139ced7c0ca3605c299bc68a00
Kael Larkin <Kael.larkin@purple.telstra.com>
2021-02-25 16:47:26 +1000

    Fixed carousel and other displays

--- static/js/Scripts.js
+++ static/js/Scripts.js
@@ -1,5 +1,25 @@
-function toggleRenderDisplay() {
-  $("#render-list-button").toggleClass("selected");
-  $("#render-tiles-button").toggleClass("selected");
-  $("#render-images").toggleClass("tiles");
+function setRenderDisplay(displayType) {
+  // Clear all first
+  $("#render-list-button").removeClass("selected");
+  $("#render-tiles-button").removeClass("selected");
+  $("#render-carousel-button").removeClass("selected");
+
+  $("#slides").removeClass("tiles");
+  $("#slides").removeClass("slides");
+
+  switch (displayType) {
+    case 'list':
+      $("#render-list-button").addClass("selected");
+      break;
+    case 'tiles':
+      $("#render-tiles-button").addClass("selected");
+      $("#slides").addClass("tiles");
+      break;
+    case 'carousel':
+      $("#render-carousel-button").addClass("selected");
+      $("#slides").addClass("slides");
+      break;
+    default:
+      break;
+  }
 }
