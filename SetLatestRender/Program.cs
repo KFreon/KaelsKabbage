16d1d49285bb7acd3b72cfb1fa9a4b5680102b3d
Kael Larkin <Kael.larkin@purple.telstra.com>
2021-03-30 20:07:31 +1000

    Fixing latest render generator

---
+++ SetLatestRender/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+// Below paths for VS
+//var renders = "../../../../Content/Renders/index.md";
+
+var renders = "../Content/Renders/index.md";
+var destination = "../static/img/recent-render.png";
+
+var latestRenderPath = File.ReadAllLines(renders)
+    .First(x => x.StartsWith("{{< image") || x.StartsWith("{{< video"))
+    .Split('"')[1];
+
+var latestImage = "../Content/Renders/" + latestRenderPath + ".png";
+var latestVideo = "../Content/Renders/" + latestRenderPath + ".webm";
+
+if (File.Exists(latestImage)) {
+    Process.Start("ffmpeg", $"-i {latestImage} -y -vf scale=300:-1 {destination}");
+} else if (File.Exists(latestVideo)) {
+    Process.Start("ffmpeg", $"-i {latestImage} -y -vframes 1 -vf scale=300:-1 {destination}");
+}
+
+Console.WriteLine("Latest Render = " + latestRenderPath);
