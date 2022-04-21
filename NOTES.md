32485e68cd02998487795078c89be71057d35ace
KFreon <kfreon@hotmail.com>
2022-04-21 14:08:01 +1000

    Adding loads of things, still lots to go

--- NOTES.md
+++ NOTES.md
@@ -1,4 +1,7 @@
-each render needs it's own full page?
-- So we can do proper deep linking, and primarily the twitter image
 Investigate Netlify large media and Git LFS so we can just save the full image in repo and dynamically serve it
 Consolidate all c# tools into one project maybe?
+
+generate postcards
+- jpgs for compat?
+- put postcard as top background on posts?
+open index.md for new renders
---
+++ tools/TEMP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+var rendersPath = "../../content/Renders";
+var renders = Directory.EnumerateFiles(rendersPath, "*.*", SearchOption.AllDirectories);
+
+var validExts = new[] {".webm", ".png"};
+
+var validRenders = renders
+  .Where(x => validExts.Contains(Path.GetExtension(x)));
+
+foreach(var render in validRenders) {
+  var folder = Path.GetDirectoryName(render);
+  var destName = Path.GetFileNameWithoutExtension(render.Replace("_VP9", "")) + "_postcard.jpg";
+  var destination = Path.Combine(folder, destName);
+
+  if (Path.GetExtension(render) == ".png") {
+    await Process.Start("ffmpeg", $"-i {render} -y -vf scale=275:-1 {destination}").WaitForExitAsync();
+  } else {
+      await Process.Start("ffmpeg", $"-i {render} -y -vframes 1 -vf scale=275:-1 {destination}").WaitForExitAsync();
+  }
+}
