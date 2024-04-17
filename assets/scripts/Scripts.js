d87715370c7607e5131b34a50f3ba8cd00fa1ca4
Kael Larkin <kael.larkin@purple.telstra.com>
2024-04-17 16:31:25 +1000

    Actually make carousel idea work for mobile only
    It's only in list view as well, as in once you drill into them, it doesn't allow expanding the image or anything.

--- assets/scripts/Scripts.js
+++ assets/scripts/Scripts.js
@@ -1,22 +1,17 @@
 function setRenderDisplay(displayType) {
   // Clear all first
-  document.getElementById("render-tiles-button").classList.remove("selected");
-  document.getElementById("render-carousel-button").classList.remove("selected");
 
   document.getElementById("slides").classList.remove("tiles");
   document.getElementById("slides").classList.remove("slides");
 
   switch (displayType) {
     case 'tiles':
-      document.getElementById("render-tiles-button").classList.add("selected");
       document.getElementById("slides").classList.add("tiles");
       break;
     case 'carousel':
-      document.getElementById("render-carousel-button").classList.add("selected");
       document.getElementById("slides").classList.add("slides");
       break;
     default:
-      document.getElementById("render-carousel-button").classList.add("selected");
       document.getElementById("slides").classList.add("tiles");
       break;
   }
