ca58eb6600a350dade06b7a0ff3950a88912df02
KFreon <KFreon@KABBAGE>
2024-12-21 14:18:13 +1000

    Add asteroid ship render

---
+++ content/renders/2025-02-19-a-bad-day-mining/index.md
@@ -0,0 +1,19 @@
+---
+title: "A bad day mining"
+date: 2025-02-19T07:54:40+10:00
+postcard: "ABadDayMining_postcard"
+slug: "a-bad-day-mining"
+---
+
+{{% render_img
+  description="A simple asteroid mining probe job becomes much more dangerous..."
+  rendertime="20 hours"
+  samples="500"
+  engine="Cycles"
+  frames="600"
+%}}
+{{< video path="img/AsteroidShip" width="1920" height="1080" alt="Asteroid ship mining operation goes wrong" >}}
+{{% /render_img %}}
+
+I'm always biting off more than I can chew these days.  
+I run out of motivation more quickly too, especially when I can't make the thing I want happen.  
--- tools/IndividualTools/FFmpeg_GenerateAllConversions.ps1
+++ tools/IndividualTools/FFmpeg_GenerateAllConversions.ps1
@@ -1,40 +1,33 @@
 param(
 [string]$output,
-[switch]$isVideo = $False,
 [string]$audio = "",
 [string]$fps = 25
 )
 
+Write-Host
+Write-Host "**Main video**"
+FFmpegConverter.ps1 -crf 25 -fps $fps -audio $audio -output $output
 
-if ($isVideo) {
-	Write-Host
-	Write-Host "**Main video**"
-	FFmpegConverter.ps1 -crf 25 -fps $fps -audio $audio -output $output
-	
-	Write-Host
-	Write-Host "**AV1**"
-	FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -av1
+Write-Host
+Write-Host "**AV1**"
+FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -av1
 
-	Write-Host
-	Write-Host "**AV1 Halfsize**"
-	FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -av1 -halfsize
+Write-Host
+Write-Host "**AV1 Halfsize**"
+FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -av1 -halfsize
 
-	Write-Host
-	Write-Host "**AV1 Quartersize**"
-	FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -av1 -smaller
+Write-Host
+Write-Host "**AV1 Quartersize**"
+FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -av1 -smaller
 
-	Write-Host
-	Write-Host "**Webm**"
-	FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -vp9
+Write-Host
+Write-Host "**Webm**"
+FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -vp9
 
-	Write-Host
-	Write-Host "**Webm Halfsize**"
-	FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -vp9 -halfsize
+Write-Host
+Write-Host "**Webm Halfsize**"
+FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -vp9 -halfsize
 
-	Write-Host
-	Write-Host "**Webm Quartersize**"
-	FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -vp9 -smaller
-}
-else{
-	Write-Host("Did you mean '-isVideo'? Can't remember why I needed it")
-}
+Write-Host
+Write-Host "**Webm Quartersize**"
+FFmpegConverter.ps1 -crf 40 -fps $fps -audio $audio -output $output -vp9 -smaller
